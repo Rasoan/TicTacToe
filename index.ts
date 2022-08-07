bc9676abe10c9f903603c678f556108cf2c51f5f
Araik Rasoyan <araikrasoian@gmail.com>
2022-08-07 13:36:50 +0300

    feat(Settings): add settinfs from game

--- index.ts
+++ index.ts
@@ -1,14 +1,24 @@
+'use strict';
+
+import {PLAYING_FIELD_DIMENSION_ID, SETTINGS_FORM_ID, WINNING_STREAK_DIMENSION_ID} from "./src/constants/constants";
+import {
+    addListenerForChangeMaxWinStreak,
+    addListenerForStartGame,
+    initializeSettingsFormFromLocalStorage
+} from "./src/SettingsForm/SettingsForm";
+
 {
-    const testElement = document.createElement('div');
-    testElement.setAttribute('id', 'testElement');
-    testElement.innerText = 'Hello World)';
+    const playingFieldDimension = document.getElementById(PLAYING_FIELD_DIMENSION_ID) as HTMLInputElement | null;
+    const winningStreakDimension = document.getElementById(WINNING_STREAK_DIMENSION_ID) as HTMLInputElement | null;
+    const settingsForm = document.getElementById(SETTINGS_FORM_ID) as HTMLFormElement | null;
 
-    const root = document.getElementById('root');
+    initializeSettingsFormFromLocalStorage(playingFieldDimension, winningStreakDimension);
 
-    if (root) {
-        root.appendChild(testElement);
-    }
-    else {
-        console.error('Root node is not defined');
-    }
+    addListenerForChangeMaxWinStreak(playingFieldDimension, winningStreakDimension);
+
+    addListenerForStartGame(
+        settingsForm,
+        playingFieldDimension,
+        winningStreakDimension
+    );
 }
---
+++ src/SettingsForm/SettingsForm.ts
@@ -0,0 +1,90 @@
+'use strict';
+
+import {getValueForLocalStorage, LocalStorage, setValueForLocalStorage} from "../localStorage/localStorage";
+
+export function addListenerForChangeMaxWinStreak(
+    playingFieldDimension: HTMLElement | null,
+    winningStreakDimension: HTMLElement | null,
+): void {
+    if (playingFieldDimension) {
+        playingFieldDimension.addEventListener('change', (event: Event) => {
+            const playingFieldDimensionValue: string = (event?.target as HTMLInputElement).value;
+
+            if (winningStreakDimension) {
+                winningStreakDimension.setAttribute('max', playingFieldDimensionValue);
+            }
+            else {
+                console.error('winningStreakDimension is not defined!');
+            }
+        });
+    }
+    else {
+        console.error('playingFieldDimension is not defined!');
+    }
+}
+
+export function initializeSettingsFormFromLocalStorage(
+    playingFieldDimension: HTMLInputElement | null,
+    winningStreakDimension: HTMLInputElement | null,
+) {
+    const playingFieldDimensionValue = getValueForLocalStorage(LocalStorage.playingFieldDimension);
+    const winningStreakDimensionValue = getValueForLocalStorage(LocalStorage.winningStreakDimension);
+
+    if (playingFieldDimension && playingFieldDimensionValue) {
+        playingFieldDimension.setAttribute('value', String(playingFieldDimensionValue));
+    }
+
+    if (winningStreakDimension && winningStreakDimensionValue) {
+        winningStreakDimension.setAttribute('value', String(winningStreakDimensionValue));
+    }
+
+    if (winningStreakDimension && playingFieldDimension) {
+        winningStreakDimension.setAttribute('max', String(playingFieldDimensionValue));
+    }
+}
+
+function _setSettingsInLocalStorage(
+    playingFieldDimension: HTMLInputElement | null,
+    winningStreakDimension: HTMLInputElement | null,
+): void {
+    const playingFieldDimensionValue = playingFieldDimension?.value;
+    const winningStreakDimensionValue = winningStreakDimension?.value;
+
+    if (playingFieldDimensionValue) {
+        setValueForLocalStorage(
+            {
+                key: LocalStorage.playingFieldDimension,
+                value: playingFieldDimensionValue,
+            },
+        );
+    }
+    else {
+        console.error('playingFieldDimension is not defined!');
+    }
+
+    if (winningStreakDimensionValue) {
+        setValueForLocalStorage(
+            {
+                key: LocalStorage.winningStreakDimension,
+                value: winningStreakDimensionValue,
+            },
+        );
+    }
+    else {
+        console.error('winningStreakDimension is not defined!');
+    }
+}
+
+export function addListenerForStartGame(
+    settingsForm: HTMLElement | null,
+    playingFieldDimension: HTMLInputElement | null,
+    winningStreakDimension: HTMLInputElement | null,
+): void {
+    if (settingsForm) {
+        settingsForm.addEventListener('submit', (event: SubmitEvent) => {
+            event.preventDefault();
+
+            _setSettingsInLocalStorage(playingFieldDimension, winningStreakDimension);
+        });
+    }
+}
