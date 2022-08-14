d4e14c9c360b8762c14c5a0f82a3d057e830889b
Araik Rasoyan <araikrasoian@gmail.com>
2022-08-14 15:16:14 +0300

    feat(jest.config): allowed import from d.ts file

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
     testMatch: [ '<rootDir>/spec/**/*_(spec|test|snap).[jt]s?(x)' ],
     testEnvironment: 'jsdom',
+    moduleFileExtensions: ["js", "mjs", "cjs", "jsx", "ts", "tsx", "json", "node", "d.ts"]
 };
